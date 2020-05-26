289d7b81ab8409e011255425cf5f3fb727be5016
ruki <waruqi@gmail.com>
2020-05-26 22:43:13 +0800

    sort targets

--- assets/targets.lua
+++ assets/targets.lua
@@ -9,9 +9,14 @@ function main ()
     config.load()
 
     -- print targets
+    local names = {}
     for name, _ in pairs((project.targets())) do 
-        print(name) 
+        table.insert(names, name)
     end 
+    table.sort(names)
+    for _, name in ipairs(names) do
+        print(name) 
+    end
 
     -- print end tag to ignore other deprecated/warnings infos
     print("__end__")
