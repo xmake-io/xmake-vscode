0abf489e9aa538b25b296e6b60b357f006cb1ef7
ruki <waruqi@gmail.com>
2020-12-11 22:56:44 +0800

    improve find_gdb

--- assets/find_gdb.lua
+++ assets/find_gdb.lua
@@ -1,15 +1,22 @@
+import("detect.sdks.find_mingw")
 import("lib.detect.find_tool")
 
 function main ()
     local paths = {}
-    if is_subhost("msys") then
-        local mingw_prefix = os.getenv("MINGW_PREFIX")
-        if mingw_prefix then
-            table.insert(paths, path.join(mingw_prefix, "bin"))
+    if is_host("windows") then
+        local mingw = find_mingw()
+        if mingw and mingw.bindir then
+            table.insert(paths, mingw.bindir)
         end
     end
     local gdb = find_tool("gdb", {norun = true, paths = paths})
     if gdb and gdb.program then
-        print(gdb.program)
+        local program = gdb.program
+        if is_host("windows") and not program:endswith(".exe") then
+            program = program .. ".exe"
+        end
+        if os.isfile(program) then
+            print(program)
+        end
     end
 end
