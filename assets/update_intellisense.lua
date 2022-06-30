3b0a9467b0448018303a9ecf0cbd309867e0c77d
ruki <waruqi@gmail.com>
2022-06-30 22:44:53 +0800

    improve compile commands

--- assets/update_intellisense.lua
+++ assets/update_intellisense.lua
@@ -7,6 +7,8 @@ function main(compileCommandsDirectory)
 
     -- generate compile_commands.json
     -- @note we can only load configuration because we watched onFileChanged(xmake.conf)
+    os.setenv("XMAKE_IN_PROJECT_GENERATOR", "true")
     config.load()
     compile_commands.make(compileCommandsDirectory or ".vscode")
+    os.setenv("XMAKE_IN_PROJECT_GENERATOR", nil)
 end
