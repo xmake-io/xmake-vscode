6ede1ed24d8fe7b0c3d31381859c79e3d42af496
A2va <49582555+A2va@users.noreply.github.com>
2022-12-18 13:49:49 +0100

    Start the resolve debug configuration

--- src/debugger.ts
+++ src/debugger.ts
@@ -1,16 +1,33 @@
 'use strict';
 
 // imports
-import * as vscode from 'vscode';
 import * as fs from 'fs';
+import * as os from 'os';
 import * as path from 'path';
+import * as vscode from 'vscode';
 import { WorkspaceFolder } from 'vscode';
 import * as process from './process';
 
-import {Option} from './option';
+import { config as settings } from './config';
 import { log } from './log';
-import { config } from './config';
+import { Option } from './option';
+import { debug } from 'console';
+import { StreamPriorityOptions } from 'http2';
 
+
+type Terminal = "integrated" | "external" | "console"
+type DebugType = "cppvsdbg" | "cppdbg" | "lldb"
+type Envs = {name: string, value: string}
+
+interface DebugConfiguration extends vscode.DebugConfiguration {
+    type: DebugType
+    target: string;
+    cwd?: string;
+    stopAtEntry?: boolean;
+    args?: Array<string> | string;
+    terminal?: Terminal;
+    environment: Array<Envs>
+}
 
 
 /**
@@ -21,7 +38,7 @@ async function findGdbPath(): Promise<string> {
     let gdbPath = null;
     let findGdbScript = path.join(__dirname, `../../assets/find_gdb.lua`);
     if (fs.existsSync(findGdbScript)) {
-        gdbPath = (await process.iorunv(config.executable, ["l", findGdbScript], { "COLORTERM": "nocolor" }, config.workingDirectory)).stdout.trim();
+        gdbPath = (await process.iorunv(settings.executable, ["l", findGdbScript], { "COLORTERM": "nocolor" }, settings.workingDirectory)).stdout.trim();
         if (gdbPath) {
             gdbPath = gdbPath.split('\n')[0].trim();
         }
@@ -37,7 +54,7 @@ async function findGdbPath(): Promise<string> {
 async function getRunDirectory(targetName: string): Promise<string> {
     let getTargetRunDirScript = path.join(__dirname, `../../assets/target_rundir.lua`);
     if (fs.existsSync(getTargetRunDirScript)) {
-        let targetRunDir = (await process.iorunv(config.executable, ["l", getTargetRunDirScript, targetName], { "COLORTERM": "nocolor" }, config.workingDirectory)).stdout.trim();
+        let targetRunDir = (await process.iorunv(settings.executable, ["l", getTargetRunDirScript, targetName], { "COLORTERM": "nocolor" }, settings.workingDirectory)).stdout.trim();
         if (targetRunDir) {
             targetRunDir = targetRunDir.split("__end__")[0].trim();
             targetRunDir = targetRunDir.split('\n')[0].trim();
@@ -56,7 +73,7 @@ async function getRunDirectory(targetName: string): Promise<string> {
 async function getProgram(targetName: string): Promise<string> {
     let getTargetPathScript = path.join(__dirname, `../../assets/targetpath.lua`);
     if (fs.existsSync(getTargetPathScript)) {
-        let targetProgram = (await process.iorunv(config.executable, ["l", getTargetPathScript, targetName], { "COLORTERM": "nocolor" }, config.workingDirectory)).stdout.trim();
+        let targetProgram = (await process.iorunv(settings.executable, ["l", getTargetPathScript, targetName], { "COLORTERM": "nocolor" }, settings.workingDirectory)).stdout.trim();
         if (targetProgram) {
             targetProgram = targetProgram.split("__end__")[0].trim();
             targetProgram = targetProgram.split('\n')[0].trim();
@@ -72,10 +89,10 @@ async function getProgram(targetName: string): Promise<string> {
  * @param targetName xmake target
  * @returns return object like { "name": "config", "value": "Debug" }
  */
-async function getEnvs(targetName: string): Promise<string> {
+async function getEnvs(targetName: string): Promise<Envs> {
     let getTargetRunEnvsScript = path.join(__dirname, `../../assets/target_runenvs.lua`);
     if (fs.existsSync(getTargetRunEnvsScript)) {
-        let targetRunEnvs = (await process.iorunv(config.executable, ["l", getTargetRunEnvsScript, targetName], { "COLORTERM": "nocolor" }, config.workingDirectory)).stdout.trim();
+        let targetRunEnvs = (await process.iorunv(settings.executable, ["l", getTargetRunEnvsScript, targetName], { "COLORTERM": "nocolor" }, settings.workingDirectory)).stdout.trim();
         if (targetRunEnvs) {
             targetRunEnvs = targetRunEnvs.split("__end__")[0].trim();
             targetRunEnvs = targetRunEnvs.split('\n')[0].trim();
@@ -91,6 +108,20 @@ async function getEnvs(targetName: string): Promise<string> {
     return null;
 }
 
+function convertEnvsToLLDB(envs: Array<Envs>) {
+    let envsLLDB = {};
+    for (let item of (envs as Array<Object>)) {
+        let map = item as Map<String, String>;
+        if (map) {
+            let name = map["name"];
+            let value = map["value"];
+            if (name && value) {
+                envsLLDB[name] = value;
+            }
+        }
+    }
+    return envsLLDB;
+}
 
 class XmakeConfigurationProvider implements vscode.DebugConfigurationProvider {
 
@@ -99,7 +130,6 @@ class XmakeConfigurationProvider implements vscode.DebugConfigurationProvider {
     constructor(option: Option) {
         this.option = option;
     }
-
 
     private getPlat(): string {
         return this.option.get<string>("plat");
@@ -112,11 +142,28 @@ class XmakeConfigurationProvider implements vscode.DebugConfigurationProvider {
      * @param token 
      * @returns the modified config to cpptols or codelldb
      */
-    public resolveDebugConfiguration(folder: WorkspaceFolder | undefined, config: vscode.DebugConfiguration, token?: vscode.CancellationToken): vscode.ProviderResult<vscode.DebugConfiguration> {
-        config.type = 'python';
-        config.name = 'Launch';
-        config.request = 'launch';
-        config.stopOnEntry = true;
+    public async resolveDebugConfiguration(folder: WorkspaceFolder | undefined, config: DebugConfiguration, token?: vscode.CancellationToken): Promise<DebugConfiguration> {
+        
+        // Set the current working directory
+        if(config.cwd == "") {
+            config.cwd = await getRunDirectory(config.target);
+        }
+
+        // Get xmake env and merge it with config envs
+        // config envs will override xmake envs
+        const xmakeEnvs = await getEnvs(config.target);
+        config.environment = {...xmakeEnvs, ...config.environment};
+
+        // Configure debugger type
+        config.type = 'cppdbg';
+        if(settings.debuggerBackend == "codelldb") {
+            config.type = 'lldb';
+            config.envs = convertEnvsToLLDB(config.environment);
+        }
+        // On windows, use vs debugger if it's not mingw
+        if(os.platform() == 'win32' && this.getPlat() != 'mingw') {
+            config.type = 'cppvsdbg';
+        }
 
         return config;
     }
