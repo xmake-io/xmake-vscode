dd06a92b4f1c88c372f16aa3a74572f767d4e5e4
ZhouGuiqing <1304806004@qq.com>
2023-12-18 21:03:12 +0800

    Before each command execution, do some checks

--- src/extension.ts
+++ src/extension.ts
@@ -2,7 +2,12 @@
 
 // imports
 import * as vscode from 'vscode';
+import * as utils from './utils';
+import * as process from './process';
+import * as fs from 'fs';
+import * as path from 'path';
 import { XMake } from './xmake';
+import { config } from './config';
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
@@ -17,8 +22,52 @@ export async function activate(context: vscode.ExtensionContext) {
 
     // register all commands of the xmake plugin
     function register(name, fn) {
+
         fn = fn.bind(xmake);
-        return vscode.commands.registerCommand(name, _ => fn(_));
+        const slot = async (target) => {
+            if (!utils.getProjectRoot()) {
+                if (!!(await vscode.window.showErrorMessage('no opened folder!',
+                    'Open a directory first!'))) {
+                    vscode.commands.executeCommand('vscode.openFolder');
+                }
+                return;
+            }
+
+
+            // check xmake
+            if (0 != (await process.runv(config.executable, ["--version"], { "COLORTERM": "nocolor" }, config.workingDirectory)).retval) {
+                if (!!(await vscode.window.showErrorMessage('xmake not found!',
+                    'Access https://xmake.io to download and install xmake first!'))) {
+                }
+                return;
+            }
+
+            // valid xmake project?
+            switch (name) {
+                case 'xmake.onCreateProject':
+                    if (fs.existsSync(path.join(config.workingDirectory, "xmake.lua"))) {
+                        if (!(await vscode.window.showErrorMessage('xmake.lua already exists!',
+                            'continue'))) {
+                            return;
+                        }
+                    }
+                    break;
+            
+                default:
+                    if (!fs.existsSync(path.join(config.workingDirectory, "xmake.lua"))) {
+                        if (!!(await vscode.window.showErrorMessage('xmake.lua not found!',
+                            'Create a new xmake project'))) {
+                            await xmake.createProject();
+                        }
+                        return;
+                    }
+                    break;
+            }
+
+            fn(target);
+        };
+
+        return vscode.commands.registerCommand(name, slot);
     }
     for (const key of [
         'onCreateProject',
