d07244f4694fbce0b42872a31e318e2c7a9afddf
ruki <waruqi@gmail.com>
2024-05-04 23:42:28 +0800

    sort toolchains

--- assets/toolchains.lua
+++ assets/toolchains.lua
@@ -8,6 +8,9 @@ function main()
         local t = os.isfile(os.projectfile()) and project.toolchain(name) or toolchain.load(name)
         table.insert(toolchains, {name, t:get("description")})
     end
+    table.sort(toolchains, function (a, b)
+        return a[1] < b[1]
+    end)
     if json.mark_as_array then
         json.mark_as_array(toolchains)
     end
